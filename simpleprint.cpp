9d4ce7bbde594ac4cb754f67c19ca5d526ae9000
Ethan <ethancao@y7mail.com>
2022-11-30 23:04:31 +1100

    simpleprint

---
+++ simpleprint.cpp
@@ -0,0 +1,10 @@
+// The library has alot of standard functions such as cout
+#include <iostream>
+// You encompass the code in a namespace in which you can use the library in
+using namespace std;
+
+int main() {
+  // c-out is how to print in c++ with the string attached
+  cout << "Hello World!";
+  return 0;
+}
